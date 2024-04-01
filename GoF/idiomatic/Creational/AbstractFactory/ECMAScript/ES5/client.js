4f22bc8b22defcb5a22249eeca41d8959c591c56
Inho Park <98461850+qamoo@users.noreply.github.com>
2024-04-01 11:43:29 +0900

    Update client.js
    translation eng to kr

--- GoF/idiomatic/Creational/AbstractFactory/ECMAScript/ES5/client.js
+++ GoF/idiomatic/Creational/AbstractFactory/ECMAScript/ES5/client.js
@@ -6,12 +6,14 @@ var getOSFactory = require('./API/factories');
 // CLIENT CODE 
 // ==============================
 
-// We can get concrete factories from the abstract factory
+// We can get concrete factories from the abstract factory  
+// 우리는 추상 팩토리로부터 구체적인 팩토리를 얻을 수 있습니다.
 var linuxFactory = getOSFactory('LINUX'),
     macFactory = getOSFactory('Mac'),
     windowsFactory = getOSFactory('windows');
 
-// Then we can get real objects from these concrete factories
+// Then we can get real objects from these concrete factories  
+// 그런 다음에 우리는 이러한 구체적인 팩토리로부터 실제 객체를 얻을 수 있습니다.
 var debian = linuxFactory('DEBIAN'),
     osx = macFactory('osX'),
     xp = windowsFactory('xp');
