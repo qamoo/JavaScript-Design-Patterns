39945d0b91140cd7041bad38dec3a39cb563bd9c
Inho Park <98461850+qamoo@users.noreply.github.com>
2024-04-01 17:26:08 +0900

    Update client.js

--- GoF/idiomatic/Structural/Composite/ECMAScript/ES5/client.js
+++ GoF/idiomatic/Structural/Composite/ECMAScript/ES5/client.js
@@ -7,6 +7,7 @@ var toys = require('./API/toys');
 // ==============================
 
 // Here we organize our toys in an optimal way
+// 여기서 우리는 장난감을 최적의 방식으로 조직합니다.
 var ball1 = toys.ball(),
     ball2 = toys.ball(),
     ball3 = toys.ball(),
@@ -19,4 +20,5 @@ bigToyBox.add(ball3);
 bigToyBox.add(smallToyBox);
 
 // Now we open our big toy box...
+// 이제 우리는 큰 장난감 상자를 엽니다...
 console.log(bigToyBox.inventory());
