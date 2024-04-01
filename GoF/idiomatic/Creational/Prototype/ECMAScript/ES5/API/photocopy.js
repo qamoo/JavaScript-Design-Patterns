f9dc3b0bfbd9e7640ce952909a85a7a62b2f0232
Inho Park <98461850+qamoo@users.noreply.github.com>
2024-04-01 14:43:33 +0900

    Update photocopy.js

--- GoF/idiomatic/Creational/Prototype/ECMAScript/ES5/API/photocopy.js
+++ GoF/idiomatic/Creational/Prototype/ECMAScript/ES5/API/photocopy.js
@@ -7,6 +7,8 @@
 module.exports = function (invoice) {
     // Here we suppose invoice properties are always enumerable, writable and configurable.
     // If not, we should use Object.defineProperty() along with Object.getOwnPropertyDescriptor().
+    // 여기서는 속성이 항상 enumerable, writable 및 configurable로 가정합니다.
+    // 그렇지 않은 경우에는 Object.defineProperty()와 Object.getOwnPropertyDescriptor()를 함께 사용해야 합니다.
     var clone = Object.create(Object.getPrototypeOf(invoice));
     for (var prop in invoice) {
         if (invoice.hasOwnProperty(prop)) {
