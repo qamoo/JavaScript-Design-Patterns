271b9043ca898cf1cf4313f20adcddbca3161ffa
Inho Park <98461850+qamoo@users.noreply.github.com>
2024-04-02 13:26:14 +0900

    Update client.js

--- GoF/idiomatic/Structural/Flyweight/ECMAScript/ES5/client.js
+++ GoF/idiomatic/Structural/Flyweight/ECMAScript/ES5/client.js
@@ -7,9 +7,10 @@ var getLinuxDistro = require('./API/flyweight');
 // ==============================
 
 // Creation of our objects through the factory
+// 팩토리를 통한 객체 생성
 var debian = getLinuxDistro("DEBIAN"),
     debianAgain = getLinuxDistro("debian");
 
 console.log(debian.boot()); // Debian is booting...
 console.log(debianAgain.boot()); // Debian is booting...
-console.log(debian === debianAgain); // true (the same object has been reused)
+console.log(debian === debianAgain); // true (the same object has been reused; 동일한 객체가 재사용되었습니다.)
